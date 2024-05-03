991bf1254943050c844759d1a75d658139809691
herya17 <hryanto960@gmail.com>
2024-05-03 17:46:18 +0700

    disable bundleAnalyzer

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,13 +1,13 @@
 const { merge } = require('webpack-merge');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const common = require('./webpack.common');
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+// const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = merge(common, {
     mode: 'production',
     devtool: 'source-map',
     plugins: [
       new CleanWebpackPlugin(),
-      new BundleAnalyzerPlugin(),
+      // new BundleAnalyzerPlugin(),
     ],
 });
