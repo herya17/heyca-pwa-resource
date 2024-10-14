54b4e3809b9ac078302fbe232634c7d4010bbf19
herya17 <hryanto960@gmail.com>
2024-10-14 13:00:47 +0700

    add lyric translate in some music

--- src/scripts/data/lyrics.js
+++ src/scripts/data/lyrics.js
@@ -4108,8 +4108,8 @@ Jodoh pasti bertemu
     title: 'Sunao Na Niji',
     text: `
 子供の頃見た虹を今見上げると
-${<span>'Sekarang, ketika aku melihat
-pelangi yang kulihat saat kecil'</span>}
+${`Sekarang, ketika aku melihat
+pelangi yang kulihat saat kecil` + 'pppppp'}
 
 七色ではなくやがて霧がかかり
 Tak ada tujuh warna, tak lama
