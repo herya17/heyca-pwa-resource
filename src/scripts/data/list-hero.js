091c20c79d6fc53acc58c558e4fd285f282c8fe2
herya17 <hryanto960@gmail.com>
2024-10-14 18:47:18 +0700

    add heros

--- src/scripts/data/list-hero.js
+++ src/scripts/data/list-hero.js
@@ -1,15 +1,15 @@
 const listHero = [
   './images/heros/heyca.webp',
   './images/heros/heyca-0.webp',
-  // './images/heros/heyca-1.webp',
-  // './images/heros/heyca-2.webp',
-  // './images/heros/heyca-3.webp',
-  // './images/heros/heyca-4.webp',
-  // './images/heros/heyca-5.webp',
-  // './images/heros/heyca-6.webp',
-  // './images/heros/heyca-7.webp',
-  // './images/heros/heyca-8.webp',
-  // './images/heros/heyca-9.webp',
+  './images/heros/heyca-1.webp',
+  './images/heros/heyca-2.webp',
+  './images/heros/heyca-3.webp',
+  './images/heros/heyca-4.webp',
+  './images/heros/heyca-5.webp',
+  './images/heros/heyca-6.webp',
+  './images/heros/heyca-7.webp',
+  './images/heros/heyca-8.webp',
+  './images/heros/heyca-9.webp',
 ];
 
 export default listHero;
