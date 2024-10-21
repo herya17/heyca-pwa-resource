d6cccfcc87f4e561d4839b013702e7af68967a64
herya17 <hryanto960@gmail.com>
2024-10-21 12:47:22 +0700

    edit sw

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,5 +1,5 @@
 import { registerRoute } from 'workbox-routing';
-import { CacheFirst, StaleWhileRevalidate, NetworkFirst } from 'workbox-strategies';
+import { CacheFirst, StaleWhileRevalidate, NetworkFirst, NetworkOnly } from 'workbox-strategies';
 import { CacheableResponsePlugin } from 'workbox-cacheable-response';
 import { ExpirationPlugin } from 'workbox-expiration';
 import { precacheAndRoute } from 'workbox-precaching';
