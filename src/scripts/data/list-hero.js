48f0c297b22f650683c2992458dc080ba80be51e
herya17 <hryanto960@gmail.com>
2024-10-21 12:19:16 +0700

    delete some of list hero

--- src/scripts/data/list-hero.js
+++ src/scripts/data/list-hero.js
@@ -3,10 +3,10 @@ const listHero = [
   './images/heros/heyca-0.webp',
   './images/heros/heyca-1.webp',
   './images/heros/heyca-2.webp',
-  './images/heros/heyca-3.webp',
-  './images/heros/heyca-4.webp',
-  './images/heros/heyca-5.webp',
-  './images/heros/heyca-6.webp',
+  // './images/heros/heyca-3.webp',
+  // './images/heros/heyca-4.webp',
+  // './images/heros/heyca-5.webp',
+  // './images/heros/heyca-6.webp',
 ];
 
 export default listHero;
