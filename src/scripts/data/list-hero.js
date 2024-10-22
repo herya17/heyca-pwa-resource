2efbd8322dbd93beed725130fdacee6b78f9044a
herya17 <hryanto960@gmail.com>
2024-10-22 12:10:21 +0700

    edit list hero

--- src/scripts/data/list-hero.js
+++ src/scripts/data/list-hero.js
@@ -1,8 +1,8 @@
 const listHero = [
   './images/heros/heyca.webp',
-  // './images/heros/heyca-0.webp',
-  // './images/heros/heyca-1.webp',
-  // './images/heros/heyca-2.webp',
+  './images/heros/heyca-0.webp',
+  './images/heros/heyca-1.webp',
+  './images/heros/heyca-2.webp',
   // './images/heros/heyca-3.webp',
   // './images/heros/heyca-4.webp',
   // './images/heros/heyca-5.webp',
