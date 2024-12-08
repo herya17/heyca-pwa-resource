d6fa940ab2ff037314f3858a71582adcab228f59
herya17 <hryanto960@gmail.com>
2024-12-08 23:39:39 +0700

    edit playlist & lyrics

--- src/scripts/data/lyrics.js
+++ src/scripts/data/lyrics.js
@@ -4101,97 +4101,174 @@ Ku 'kan memilikimu
 Jodoh pasti bertemu
     `,
   },
-//   {
-//     id: 49,
-//     title: 'Sunao Na Niji',
-//     text: `
-// 子供の頃見た虹を今見上げると
+  {
+    id: 49,
+    title: 'Sunao Na Niji',
+    text: `
+子供の頃見た虹を今見上げると
+Sekarang, ketika aku melihat
+pelangi yang kulihat saat kecil
 
-// 七色ではなくやがて霧がかかり
+七色ではなくやがて霧がかかり
+Tak ada tujuh warna, tak lama
+kemudian mulai berkabut
 
-// 時間は過ぎ変わってゆく
+時間は過ぎ変わってゆく
+Waktu berlalu dan terus berubah
 
-// 君を探しても何処にもいない
+君を探しても何処にもいない
+Meskipun aku mencarimu, kau tak
+ada dimanapun
 
-// 目を閉じ心開いてまた眺めて見ても
+目を閉じ心開いてまた眺めて見ても
+Kututup mataku dan kubuka hatiku
+, meskipun aku menatapnya lagi
 
-// 何も変わらないのか確かめてみたい
+何も変わらないのか確かめてみたい
+Akankah ada yang berubah? Aku
+ingin memastikannya
 
-// 素直になれれば
+素直になれれば
+Jika aku terbiasa menjadi jujur
 
-// この霧が晴れると心に願い そう 叫んでる
+この霧が晴れると心に願い そう 叫んでる
+Aku berharap dalam hatiku semoga
+kabut ini akan hilang, begitulah
+teriakku
 
-// 何かを失くしてしまった事で
+何かを失くしてしまった事で
+Jika kau kehilangan sesuatu
 
-// そう 手にしたものもきっとあるんだろう
+そう 手にしたものもきっとあるんだろう
+Pasti kau juga mendapatkan
+sesuatu, bukan?
 
-// だけどまだ僕等は大人に戸惑い
+だけどまだ僕等は大人に戸惑い
+Tapi, kita masih bingung dengan
+menjadi orang dewasa
 
-// 生きてゆく術探してる
+生きてゆく術探してる
+Kita mencari cara untuk terus
+hidup
 
-// \u266A
+\u266A
 
-// 雨が止むと不意に君は歩道橋に上り
+雨が止むと不意に君は歩道橋に上り
+Ketika hujan berhenti, kau tiba
+-tiba menyeberangi jembatan
+penyeberangan
 
-// 空を指差して無邪気に笑ってた
+空を指差して無邪気に笑ってた
+Kau menunjuk ke langit dengan
+tawa yang polos
 
-// 消え入るような透けた虹を
+消え入るような透けた虹を
+Pelangi yang memudar seolah
+menghilang
 
-// 何も言えないまま眺めていたね
+何も言えないまま眺めていたね
+Kita berdua menatapnya tanpa
+mengatakan apapun
 
-// 本当は あの時ずっと
+本当は あの時ずっと
+Sebenarnya, saat itu, selalu
 
-// 君の横顔を見る事に夢中になってた
+君の横顔を見る事に夢中になってた
+Aku menjadi tergila-gila untuk
+melihat wajah sampingmu
 
-// 忘れない為に
+忘れない為に
+Agar aku tak bisa melupakannya
 
-// 素直になれずに
+素直になれずに
+Aku tak bisa menjadi orang yang
+jujur
 
-// ただ君の名前を 心の中でそう叫んでる
+ただ君の名前を 心の中でそう叫んでる
+Aku hanya meneriakkan namamu
+di dalam hatiku
 
-// 君を失ってまで手にしたいものなど
+君を失ってまで手にしたいものなど
+Padahal seharusnya tak ada yang
+ingin kudapatkan
 
-// 無い筈なのにどうしてだろう
+無い筈なのにどうしてだろう
+Bahkan jika aku kehilanganmu,
+tapi kenapa?
 
-// 濡れた髪を伝い落ちるのは
+濡れた髪を伝い落ちるのは
+Yang mengalir di rambutku yang
+basah adalah
 
-// 「さよなら」 という涙だったね
+「さよなら」 という涙だったね
+Air mata yang bernama
+"selamat tinggal"
 
-// 雨上がりは今も
+雨上がりは今も
+Bahkan sekarang, setelah hujan
 
-// 歩道橋から狭い空見上げ
+歩道橋から狭い空見上げ
+Aku menatap langit yang sempit
+dari jembatan penyeberangan
 
-// 架け橋を探すけど
+架け橋を探すけど
+Meskipun aku mencarimu dijalan
+setapak jembatan penyeberangan
 
-// 君の元へ辿り着けないんだ
+君の元へ辿り着けないんだ
+Aku tak bisa mencapai tempatmu
 
-// モノクロの世界にいる
+モノクロの世界にいる
+Aku berada di dunia hitam putih
 
-// \u266A
+\u266A
 
-// 言葉に出来ずに
+言葉に出来ずに
+Kata-kata yang tak bisa kuucapkan
 
-// 閉じ込めた思いを今更君に問いかけてみる
+閉じ込めた思いを今更君に問いかけてみる
+Dan perasaan yang kupendam, akan
+kucoba untuk bertanya padamu
+sekarang
 
-// 弱さのせいにして逃げ出したのは
+弱さのせいにして逃げ出したのは
+Melarikan diri dan menyalahkan
+kelemahan
 
-// そう 君の方じゃない僕の方だよ
+そう 君の方じゃない僕の方だよ
+Benar, itu bukanlah jalamu, tapi
+jalanku
 
-// 素直になれれば
+素直になれれば
+Jika aku terbiasa menjadi jujur
 
-// この霧が晴れると 心に願い そう 叫んでる
+この霧が晴れると 心に願い そう 叫んでる
+Aku berharap dalam hatiku semoga
+kabut ini akan hilang, begitulah
+teriakku
 
-// 何か足りなくても今ある色で
+何か足りなくても今ある色で
+Meskipun aku tak cukup dalam
+beberapa hal, tapi dengan warna
+yang kupunya sekarang
 
-// そう 僕の世界を塗り替えてみたい
+そう 僕の世界を塗り替えてみたい
+Benar, aku ingin coba mengecat
+ulang duniaku
 
-// いつかまた君に会えるなら
+いつかまた君に会えるなら
+Jika suatu hari nanti kita
+bertemu lagi
 
-// 素直に思い伝えるから
+素直に思い伝えるから
+Aku akan mengungkapkan
+perasaanku dengan jujur
 
-// 素直な虹を描いてみるから
-//     `,
-//   },
+素直な虹を描いてみるから
+Karena aku akan mencoba
+menggambar pelangi kejujuran
+    `,
+  },
 //   {
 //     id: 50,
 //     title: 'Monochrome',
@@ -4272,6 +4349,41 @@ Jodoh pasti bertemu
 //   },
   {
     id: 51,
+    title: 'Freesia',
+    text: `
+Soon
+    `,
+  },
+  {
+    id: 52,
+    title: 'Remember',
+    text: `
+Soon
+    `,
+  },
+  {
+    id: 53,
+    title: 'Anataga Iru Kotode',
+    text: `
+Soon
+    `,
+  },
+  {
+    id: 54,
+    title: 'Musume Yori',
+    text: `
+Soon
+    `,
+  },
+  {
+    id: 55,
+    title: 'Soreo Aito Yobunara',
+    text: `
+Soon
+    `,
+  },
+  {
+    id: 56,
     title: 'Sukiga Afureteitano',
     text: `
 ねえ あなたは幸せだった?
@@ -4350,7 +4462,7 @@ Jodoh pasti bertemu
     `,
   },
   {
-    id: 52,
+    id: 57,
     title: 'Gravity',
     text: `
 帰ろうとしない帰り道 いつもどおり
@@ -4442,7 +4554,7 @@ Jodoh pasti bertemu
     `,
   },
   {
-    id: 53,
+    id: 58,
     title: 'Sakura Koi',
     text: `
 おはよう の度また惹かれてく
@@ -4519,7 +4631,7 @@ Jodoh pasti bertemu
     `,
   },
   {
-    id: 54,
+    id: 59,
     title: 'Hoshi No Utsuwa',
     text: `
 どうして どうして 泣いて生まれて
@@ -4600,164 +4712,87 @@ Jodoh pasti bertemu
 export default lyrics;
 
 // 子供の頃見た虹を今見上げると
-// Sekarang, ketika aku melihat
-// pelangi yang kulihat saat kecil
 
 // 七色ではなくやがて霧がかかり
-// Tak ada tujuh warna, tak lama
-// kemudian mulai berkabut
 
 // 時間は過ぎ変わってゆく
-// Waktu berlalu dan terus berubah
 
 // 君を探しても何処にもいない
-// Meskipun aku mencarimu, kau tak
-// ada dimanapun
 
 // 目を閉じ心開いてまた眺めて見ても
-// Kututup mataku dan kubuka hatiku
-// , meskipun aku menatapnya lagi
 
 // 何も変わらないのか確かめてみたい
-// Akankah ada yang berubah? Aku
-// ingin memastikannya
 
 // 素直になれれば
-// Jika aku terbiasa menjadi jujur
 
 // この霧が晴れると心に願い そう 叫んでる
-// Aku berharap dalam hatiku semoga
-// kabut ini akan hilang, begitulah
-// teriakku
 
 // 何かを失くしてしまった事で
-// Jika kau kehilangan sesuatu
 
 // そう 手にしたものもきっとあるんだろう
-// Pasti kau juga mendapatkan
-// sesuatu, bukan?
 
 // だけどまだ僕等は大人に戸惑い
-// Tapi, kita masih bingung dengan
-// menjadi orang dewasa
 
 // 生きてゆく術探してる
-// Kita mencari cara untuk terus
-// hidup
 
 // \u266A
 
 // 雨が止むと不意に君は歩道橋に上り
-// Ketika hujan berhenti, kau tiba
-// -tiba menyeberangi jembatan
-// penyeberangan
 
 // 空を指差して無邪気に笑ってた
-// Kau menunjuk ke langit dengan
-// tawa yang polos
 
 // 消え入るような透けた虹を
-// Pelangi yang memudar seolah
-// menghilang
 
 // 何も言えないまま眺めていたね
-// Kita berdua menatapnya tanpa
-// mengatakan apapun
 
 // 本当は あの時ずっと
-// Sebenarnya, saat itu, selalu
 
 // 君の横顔を見る事に夢中になってた
-// Aku menjadi tergila-gila untuk
-// melihat wajah sampingmu
 
 // 忘れない為に
-// Agar aku tak bisa melupakannya
 
 // 素直になれずに
-// Aku tak bisa menjadi orang yang
-// jujur
 
 // ただ君の名前を 心の中でそう叫んでる
-// Aku hanya meneriakkan namamu
-// di dalam hatiku
 
 // 君を失ってまで手にしたいものなど
-// Padahal seharusnya tak ada yang
-// ingin kudapatkan
 
 // 無い筈なのにどうしてだろう
-// Bahkan jika aku kehilanganmu,
-// tapi kenapa?
 
 // 濡れた髪を伝い落ちるのは
-// Yang mengalir di rambutku yang
-// basah adalah
 
 // 「さよなら」 という涙だったね
-// Air mata yang bernama
-// "selamat tinggal"
 
 // 雨上がりは今も
-// Bahkan sekarang, setelah hujan
 
 // 歩道橋から狭い空見上げ
-// Aku menatap langit yang sempit
-// dari jembatan penyeberangan
 
 // 架け橋を探すけど
-// Meskipun aku mencarimu dijalan
-// setapak jembatan penyeberangan
 
 // 君の元へ辿り着けないんだ
-// Aku tak bisa mencapai tempatmu
 
 // モノクロの世界にいる
-// Aku berada di dunia hitam putih
 
 // \u266A
 
 // 言葉に出来ずに
-// Kata-kata yang tak bisa kuucapkan
 
 // 閉じ込めた思いを今更君に問いかけてみる
-// Dan perasaan yang kupendam, akan
-// kucoba untuk bertanya padamu
-// sekarang
 
 // 弱さのせいにして逃げ出したのは
-// Melarikan diri dan menyalahkan
-// kelemahan
 
 // そう 君の方じゃない僕の方だよ
-// Benar, itu bukanlah jalamu, tapi
-// jalanku
 
 // 素直になれれば
-// Jika aku terbiasa menjadi jujur
 
 // この霧が晴れると 心に願い そう 叫んでる
-// Aku berharap dalam hatiku semoga
-// kabut ini akan hilang, begitulah
-// teriakku
 
 // 何か足りなくても今ある色で
-// Meskipun aku tak cukup dalam
-// beberapa hal, tapi dengan warna
-// yang kupunya sekarang
 
 // そう 僕の世界を塗り替えてみたい
-// Benar, aku ingin coba mengecat
-// ulang duniaku
 
 // いつかまた君に会えるなら
-// Jika suatu hari nanti kita
-// bertemu lagi
 
 // 素直に思い伝えるから
-// Aku akan mengungkapkan
-// perasaanku dengan jujur
 
 // 素直な虹を描いてみるから
-// Karena aku akan mencoba
-// menggambar pelangi kejujuran
