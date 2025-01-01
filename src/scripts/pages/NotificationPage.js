59460a106b68949f254236c3573ebaa99950f3c8
herya17 <hryanto960@gmail.com>
2025-01-01 22:06:03 +0700

    add msg NotificationPage

--- src/scripts/pages/NotificationPage.js
+++ src/scripts/pages/NotificationPage.js
@@ -17,7 +17,7 @@ function DetailPage() {
     <section>
       <div className='note-detail'>
       <p className='note-detail__title'>Who???</p>
-      <p className='note-detail__date'>00:21. Rabu, 25 Desember 2024</p>
+      <p className='note-detail__date'>00:21. Rabu, 25 Desember 2024 & <i>22:10. Rabu, 1 Januari 2025</i></p>
       <p className='note-detail__body'>
         Maaf kalo keberadaanku mengganggu buat kamu ya Ca. Tanpa disadari aku telah mengetahui banyak hal tentang kamu,
         tentang mereka yg pernah sangat dekat dengan kamu, sampai aku pernah berfikir apa yg kamu lakukan ke mereka sampai mereka mau seperti itu kepadamu ataupun sebaliknya. 
@@ -26,8 +26,8 @@ function DetailPage() {
         Tak terasa sudah sangat lama aku merasakan hal yang sangat tidak menyenangkan dalam hati yang penuh bergejolak dengan rasa marah, cemas, tanpa ada henti yg terkadang emosi ini gak bisa aku tahan.
         Aku sempat berandai kapan ini akan berakhir, perasaan yg gak nyaman ini. Aku tinggal di tempat kelahiranku disini dan ini baru pertama kali nya aku merasakan seperti bukan tinggal di rumah dimana aku bisa pulang dengan nyaman.
         Aku sampai berkeingin untuk keluar dari rumah ini, dan tidak pernah kembali dan mencari rumah baru yg nyaman untuk pulang.
-        Ohya aku terkadang mengetahui pesan yg kamu sampaikan entah ini hanya firasat atau memang hanya kebetulan, aku gk bisa memahaminya. Kamu mungkin cuman gak tega karena aku telah melakukan beberapa hal, mungkin sekarang aku telah mengetahuinya kalo perasaan kamu bukan untukku, atau mungkin sejak dulu.
-        Dari kenyataan ini aku dapat melihat kalo hati seseorang gak bisa di pakasain.<br /><br />
+        Ohya aku terkadang mengetahui pesan yg kamu sampaikan entah ini hanya firasat atau memang hanya kebetulan, aku gk bisa memahaminya. Kamu mungkin cuman gak tega karena aku telah melakukan beberapa hal, mungkin sekarang aku telah mengetahuinya kalo perasaan kamu bukan untukku, atau mungkin sejak dulu.<br />
+        Dari kenyataan ini aku dapat melihat kalo hati seseorang gak bisa di pakasain. <i>Mungkin kamu saat ini sedang di dekati oleh yang lain kan, kamu memang cantik begitu banyak yang ingin bersama kamu dan menjaga kamu. Ternyata yang kamu cari itu bukan aku, engga ada satu pun.</i><br /><br />
         Di hari Natal yg suci ini aku berharap kamu bisa berubah dari hal-hal yg banyak ingin aku perbaiki dari kamu, ada banyak hal yg ingin aku protes ke kamu, banyak bangett. 
         Dan pada Natal ini aku dari dulu hanya berharapkan semua hal yg aku khawatirkan benar tidak adanya. 
         Dan semoga Tuhan memberikan kita anugerah untuk menjadi pribadi yg lebih baik lagi.
